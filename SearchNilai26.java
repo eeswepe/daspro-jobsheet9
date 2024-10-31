59c762bfa9f6cf5f897d6100e3baa36f31320f7b
eeswepe <singgihwahyupermana@gmail.com>
2024-10-31 08:57:37 +0700

    feat: finishing fourth subtask

--- SearchNilai26.java
+++ SearchNilai26.java
@@ -1,18 +1,32 @@
+import java.util.Scanner;
+
 public class SearchNilai26 {
     public static void main(String[] args) {
-        int[] arrNilai = {80,85,78,96,90,82,86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan di input : ");
+        int n = sc.nextInt();
+        
+        int[] arrNilai = new int[n];
 
         for(int i=0;i<arrNilai.length;i++) {
-            if(key == arrNilai[i]) {
-                hasil = i;
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        int key = sc.nextInt();
+        System.out.println();
+
+        boolean ketemu = false;
+        for(int i=0;i<arrNilai.length;i++) {
+            if (arrNilai[i] == key) {
+                System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (i+1));
+                ketemu = true;
                 break;
             }
         }
-
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if(!ketemu){
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
     }   
 }
